83bf4155c42043d0b25c2e32afe3da0afef6857e
princeantala45 <princeantala45@gmail.com>
2026-08-23 16:00:44 +0530

    second commit

---
+++ accounts/urls.py
@@ -0,0 +1,57 @@
+from django.urls import path
+
+from .views import (
+    login_view,
+    signup_view,
+    uptime_check,
+    check_device,
+    song_suggestion,
+    favorites_list,
+    favorite_toggle,
+)
+
+
+urlpatterns = [
+
+    path(
+        "login/",
+        login_view,
+        name="login"
+    ),
+
+    path(
+        "signup/",
+        signup_view,
+        name="signup"
+    ),
+
+    path(
+        "check-device/",
+        check_device,
+        name="check_device"
+    ),
+
+    path(
+        "uptime/",
+        uptime_check,
+        name="uptime_check"
+    ),
+
+    path(
+        "suggestions/",
+        song_suggestion,
+        name="song_suggestion"
+    ),
+
+    path(
+        "favorites/",
+        favorites_list,
+        name="favorites_list"
+    ),
+
+    path(
+        "favorites/toggle/",
+        favorite_toggle,
+        name="favorite_toggle"
+    ),
+]
