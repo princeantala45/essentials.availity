ad2560aa72749e7770306a472d17f78bc6b34b68
princeantala45 <princeantala45@gmail.com>
2026-09-20 21:03:42 +0530

    SOME GENERAL CHECKINGS

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&(u&v&@^(f)uy1&r1y-+4(u43^!$zj%7c(9)103h*6!4fa=u7g
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "https://essentials-availity.onrender.com",
+    "essentials-availity.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
@@ -160,6 +160,8 @@ CSRF_TRUSTED_ORIGINS = [
 
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    
+    "https://essentials-availity.onrender.com"
 ]
 
 JAZZMIN_SETTINGS = {
